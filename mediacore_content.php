67fa6f391342cad5ebc20d53fe992ce6d561f38c
mcgourtyalex <mcgourty.alex@berkeley.edu>
2014-07-09 13:41:34 -0700

    readability improved

--- mediacore_content.php
+++ mediacore_content.php
@@ -3,15 +3,25 @@
 // Populate the widget:
 function mediacore_test_content() {
 
+    // Create new document and load in the ATube RSS
     $dom = new DOMDocument();
     $xml = $dom->load("http://atube/latest.xml");
+
+    // Arrays for info on each RSS elem
+    // Links to mp4
     $links = [];
+    // Title of videos
     $titles = [];
+    // Links to video page
     $hrefs = [];
 
+    // incrementer
     $spot = 0;
+
+    // If the xml dom object is accessible
     if ($xml) {
 
+        // get each item in the XML file and iterate
         $items = $dom->getElementsByTagName('item');
         foreach($items as $item) {
             
@@ -21,9 +31,7 @@ function mediacore_test_content() {
 
             // Pull the mp4 links out
             $link = $item->getElementsByTagNameNS('*','content')->item(0)->getAttribute('url');
-            $link = str_replace('?download=1', "", $link);
-            $link = 'https://atube.autodesk.com'.$link;
-            $links[$spot] = $link;
+            $links[$spot] = linkify($link);
 
             // Pull the hrefs out
             $href = $item->getElementsByTagName('link')->item(0)->nodeValue;
@@ -33,9 +41,11 @@ function mediacore_test_content() {
             $spot++;
         }
 
-
+        // Echo the header
         echo "<h2>Latest ATube Videos:</h2>";
+        // Echo the table to hold 4 videos
         echo '<table style="border: none;">';
+        // Video TR
         echo '<tr>';
         for ($i = 0; $i < 4; $i++){
             echo '<td style="border: none; text-align: center;">';
@@ -43,10 +53,13 @@ function mediacore_test_content() {
             echo '</td>';
         }
         echo '</tr>';
+        // Title TR
         echo '<tr>';
         for ($i = 0; $i < 4; $i++){
             echo '<td style="border: none; text-align: center;">';
+            // Link with href
             echo '<a href="'.$hrefs[$i].'">';
+            // Echo title
             echo '<h5>'.$titles[$i].'</h5>';
             echo '</a>';
             echo '</td>';
@@ -55,9 +68,11 @@ function mediacore_test_content() {
         
         echo '</table>';
 
+        // More
         echo '<a href="https://atube.autodesk.com/media?show=latest"><h4>> More on Atube </h4></a>';
         
     } else {
+        // Echo if RSS can't be reached
         echo "Whoops! RSS unavailable.";
     }
 
@@ -68,6 +83,12 @@ function embedify($link) {
     <source src="'.$link.'" type="video/mp4">
     </video> ';
 }
+
+function linkify($link) {
+    $link = str_replace('?download=1', "", $link);
+    return 'https://atube.autodesk.com'.$link;
+}
+
 
 
 ?>
